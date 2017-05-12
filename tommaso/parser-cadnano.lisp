dc61614cf84169a2eec97557f3c44f12816c7ec3
drmeister <chris.schaf@verizon.net>
2017-05-12 14:36:09 -0400

    Got the code in parser-cadnano.lisp working

--- tommaso/parser-cadnano.lisp
+++ tommaso/parser-cadnano.lisp
@@ -1,6 +1,9 @@
 
+(ql:quickload "cl-json")
+
+
 (defun LIST-OF-VSTRANDS-FROM-JSON (json)
-  (cdr (assoc "vstrands" json)))
+  (cdr (assoc :vstrands json)))
 
 
 (defclass vstrand ()
@@ -20,21 +23,21 @@
 (defun parse-json (json)
   (let ((vstrands (make-hash-table :test #'eql)))
     (loop for json-vstrand-info in (LIST-OF-VSTRANDS-FROM-JSON json)
-       for num = (cdr (assoc "num" json-vstrand-info))
-       for col = (cdr (assoc "col" json-vstrand-info))
-       for row = (cdr (assoc "row" json-vstrand-info))
-       for staple-json = (cdr (assoc "stap" json-vstrand-info))
-       for scaffold-json = (cdr (assoc "scaf" json-vstrand-info))
-       for skip = (cdr (assoc "skip" json-vstrand-info))
-       for loop = (cdr (assoc "loop" json-vstrand-info))
-       for scaf-loop = (cdr (assoc "scafLoop" json-vstrand-info))
-       for stap-loop = (cdr (assoc "stapLoop" json-vstrand-info))
+       for num = (cdr (assoc :num json-vstrand-info))
+       for col = (cdr (assoc :col json-vstrand-info))
+       for row = (cdr (assoc :row json-vstrand-info))
+       for staple-json = (cdr (assoc :stap json-vstrand-info))
+       for scaffold-json = (cdr (assoc :scaf json-vstrand-info))
+       for skip = (cdr (assoc :skip json-vstrand-info))
+       for loop = (cdr (assoc :loop json-vstrand-info))
+       for scaf-loop = (cdr (assoc :scafLoop json-vstrand-info))
+       for stap-loop = (cdr (assoc :stapLoop json-vstrand-info))
        for vstrand = (progn
-		       (or (= (length staple) (length scaffold))
+		       (or (= (length staple-json) (length scaffold-json))
 			   (error "In vstrand ~a the staple length ~a and the scaffold length ~a are not the same - and they must be"
 				  num
-				  (length staple)
-				  (length scaffold)))
+				  (length staple-json)
+				  (length scaffold-json)))
 		       (make-instance 'vstrand
 				      :staple-json staple-json
 				      :scaffold-json scaffold-json
@@ -46,11 +49,12 @@
 				      :scaffold-vec (make-array (length scaffold-json))
 				      :row row
 				      :column col))
+       do (format t "vstrand -> ~a~%" vstrand)
        do (setf (gethash num vstrands) vstrand)
-;       do (BUILD-NODE (staple-vec vstrand) staple-json)
-       do (BUILD-NODE (staple-vec vstrand))	 
-;       do (BUILD-NODE (scaffold-vec vstrand) scaffold-json)
-       do (BUILD-NODE (scaffold-vec vstrand))
+					;       do (BUILD-NODE (staple-vec vstrand) staple-json)
+       do (BUILD-NODE staple-json (staple-vec vstrand))	 
+					;       do (BUILD-NODE (scaffold-vec vstrand) scaffold-json)
+       do (BUILD-NODE scaffold-json (scaffold-vec vstrand))
        do (intra-helix-connect (staple-vec vstrand) (scaffold-vec vstrand)))
     ;; connect the nodes
     (loop for vstrand being the hash-values in vstrands using (hash-key num)
@@ -61,26 +65,39 @@
        do (CONNECT-EVERYTHING vstrands num staple-json staple-vec #'staple-vec)
        do (CONNECT-EVERYTHING vstrands num scaffold-json scaffold-vec #'scaffold-vec))
     ;; Deal with loop
-    ))
+    vstrands))
     
-(defun BUILD-NODE (vec)
-  (loop for index from 1 to (length vec)
+(defun BUILD-NODE (strand-json vec)
+  (loop for index from 0 below (length vec)
+     for node-json in strand-json
+     unless (apply #'= -1 node-json)
      do (setf (elt vec index) (make-instance 'node))))
 
 (defun INTRA-HELIX-CONNECT (staple-vec scaffold-vec)
-  (loop for index from 1 to (length staple-vec)
-     do (setf (hbond-node (elt staple-vec index)) (elt scaffold-vec index))
-     do (setf (hbond-node (elt scaffold-vec index)) (elt staple-vec index))))
+  (loop for index from 0 below (length staple-vec)
+     for staple-node = (elt staple-vec index)
+     for scaffold-node = (elt scaffold-vec index)
+     do (when (and staple-node scaffold-node)
+	  (setf (hbond-node staple-node) scaffold-node)
+	  (setf (hbond-node scaffold-node) staple-node))))
     
-(defun CONNECT-EVERYTHING (vstrands num json vec accessor)
-  (loop for json-info in json
-     for index from 0 below (length json)
-     for node = (elt vec index)
+(defun CONNECT-EVERYTHING (vstrands num strand-json strand-vec accessor)
+  (loop for json-info in strand-json
+     for index from 0 below (length strand-json)
+     for node = (elt strand-vec index)
      when node
-     for forward-node = (LOOKUP-NODE vstrands (first json-info) (second json-info) accessor)
-     for back-node = (LOOKUP-NODE vstrands (third json-info) (fourth json-info) accessor)
-     do (when forward-node (setf (forward-node node) forward-node))
-     do (when back-node (setf (back-node node) back-node))))
+     do (let* ((fwd-vstrand (first json-info))
+	       (fwd-pos     (second json-info))
+	       (bkd-vstrand (third json-info))
+	       (bkd-pos     (fourth json-info)))
+	  (when (/= fwd-vstrand -1)
+	    (let ((forward-node (LOOKUP-NODE vstrands fwd-vstrand fwd-pos accessor)))
+	      (assert forward-node)
+	      (setf (forward-node node) forward-node)))
+	  (when (/= bkd-vstrand -1)
+	    (let ((back-node (LOOKUP-NODE vstrands bkd-vstrand bkd-pos accessor)))
+	      (assert back-node)
+	      (setf (back-node node) back-node))))))
 	      
 (defclass node ()
   ((hbond-node :initarg :hbond-node :accessor hbond-node)
@@ -93,3 +110,17 @@
     (elt strand pos)))
 
     
+#| testing code
+
+(defparameter result (parse-json *j*))
+
+result
+
+(/= -1 -1 -2 -3 0)
+
+|#
+
+
+
+
+
